4953c01d4be43ffe5671494d5a7023efc97a0c2b
panyazhong <pyz1226149198@gmail.com>
2018-06-12 14:41:46 +0800

    modify login api

--- src/api/login.js
+++ src/api/login.js
@@ -2,7 +2,7 @@ import axiosJSON from '@/common/js/axiosJSON'
 import {URL} from '@/api/config'
 
 export function login(data) {
-  let url = URL + 'auth/login'
+  let url = URL + 'login'
   let method = 'POST'
   return axiosJSON(method, url, data)
 }
